814b99341075b2760aa5ff044cdfff4ccb5435cb
Alexodm <alexodm@gmail.com>
2019-12-02 20:05:13 +0600

    Auth tests commit

--- V.0/src/test/java/ApiTests/AuthTests.java
+++ V.0/src/test/java/ApiTests/AuthTests.java
@@ -9,10 +9,11 @@ import org.apache.http.HttpStatus;
 import org.junit.*;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class AuthTests {
-    private RequestSpecification baseSpec(){
+    private RequestSpecification baseSpec() {
         return new RequestSpecBuilder()
                 .setContentType(ContentType.JSON.withCharset("UTF-8"))
                 .setBaseUri("http://httpbin.org/")
@@ -23,18 +24,47 @@ public class AuthTests {
     }
 
     @Test
-    public void checkGetMethod(){
-        given().auth().preemptive().basic("user", "passwd")
-                .when().get("http://httpbin.org/basic-auth/undefined/undefined")
+    public void checkBasicAuth() {
+        given().spec(baseSpec()).auth().basic("user", "passwd")
+                .when().get("basic-auth/user/passwd")
                 .then().statusCode(HttpStatus.SC_OK);
+    }
 
+    @Test
+    public void checkBearerAuth() {
+        given().spec(baseSpec()).auth().preemptive().oauth2("Authorisation")
+                .when().get("bearer")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
 
-//        given().auth()
-//                .basic("user", "passwd")
-//                .when()
-//                .get("http://httpbin.org/basic-auth/undefined/undefined")
-//                .then()
-//                .statusCode(HttpStatus.SC_OK);
+    @Test
+    public void checkDigestAuth() {
+        given().spec(baseSpec()).auth().digest("user", "passwd")
+                .when().get("digest-auth/undefined/user/passwd")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void checkDigestAuthWithAlg() {
+        given().spec(baseSpec()).auth().digest("user", "passwd")
+                .when().get("digest-auth/undefined/user/passwd/MD5")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void checkDigestAuthWithStale() {
+        given().spec(baseSpec()).auth().digest("user", "passwd")
+                .when().get("digest-auth/undefined/user/passwd/MD5/never")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void checkHiddenBasicAuth() {
+        given().spec(baseSpec()).auth().preemptive().basic("user", "passwd")
+                .formParam("user", "user")
+                .formParam("passwd", "passwd")
+                .when().get("hidden-basic-auth/user/passwd/")
+                .then().statusCode(HttpStatus.SC_OK);
     }
 
 }
